759d464354c25763dc22fe1fd96a68006e876a05
“together08” <together08@yeah.net>
2021-01-22 10:47:49 +0800

    sdm845: Common device tree bringup

--- extract-files.sh
+++ extract-files.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
@@ -19,8 +18,8 @@
 
 set -e
 
-DEVICE=m1882
 VENDOR=meizu
+DEVICE_COMMON=sdm845
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -33,12 +32,13 @@ if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-source "${HELPER}"
+. "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 SECTION=
 KANG=
+DEVICE=
 
 while [ "$1" != "" ]; do
     case "$1" in
@@ -50,11 +50,21 @@ while [ "$1" != "" ]; do
                                 SECTION="$1"
                                 CLEAN_VENDOR=false
                                 ;;
+        --m1882 )               DEVICE=m1882
+                                ;;
+        --m1892 )               DEVICE=m1892
+                                ;;
         * )                     SRC="$1"
                                 ;;
     esac
     shift
 done
+
+if [ -z "${DEVICE}" ]; then
+    echo "The device name was not selected!"
+    echo "Use --m1882 (if 16th) or --m1892 (if 16thPlus)!"
+    exit 1
+fi
 
 if [ -z "${SRC}" ]; then
     SRC=adb
@@ -62,15 +72,30 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+    vendor/bin/hw/vendor.display.color@1.0-service | vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service | vendor/lib/vendor.display.postproc@1.0_vendor.so)
+        patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
+        ;;
+
     vendor/lib64/hw/vendor.qti.hardware.sensorscalibrate@1.0-impl.so)
         sed -i "s|libbase.so|libbv28.so|g" "${2}"
+        ;;
+
+    vendor/lib/hw/camera.qcom.so)
+        sed -i "s|libssc.so|libSSc.so|g" "${2}"
         ;;
     esac
 }
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files-common.txt" "${SRC}" ${KANG} --section "${SECTION}"
 
-"${MY_DIR}/setup-makefiles.sh"
+if [ -n "${DEVICE}" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+
+    extract "${MY_DIR}/${DEVICE}/proprietary-files-${DEVICE}.txt" "${SRC}"
+fi
+
+"${MY_DIR}/setup-makefiles.sh" "--${DEVICE}"
